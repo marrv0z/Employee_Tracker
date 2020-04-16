1226b139a705395aa9e429a3c9fb51a26240d9fc
marrv0z <marrv00@outlook.com>
2020-04-15 23:17:19 -0500

    final steps

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//var express = require("express");
-//var app = express();
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
@@ -20,13 +18,17 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
+console.log(
+    `EMPLOYEE TRACKER`
+);
+
 init();
 
 function init(){
     inquirer.prompt(
     {
         type: "list",
-        mssage: "What would you like to do?",
+        message: "What would you like to do?",
         choices:[
             'View all employees',
             'View all departments',
@@ -62,7 +64,8 @@ function init(){
                 updateEmpRol();
                 break;
             case 'Update an employee manager':
-                updateEmpMan();
+                unavailableRN();
+                //updateEmpMan();
                 break;
             case 'Add an employee':
                 addEmployee();
@@ -80,19 +83,23 @@ function init(){
                 viewEmpRol();
                 break;
             case 'View employees by manager':
-                viewEmpMan();
+                unavailableRN();
+                //viewEmpMan();
                 break;
             case 'Remove department':
-                removeDept();
+                unavailableRN();
+                //removeDept();
                 break;
             case 'Remove employee':
                 removeEmp();
                 break;
             case 'Remove role':
-                removeRole();
+                unavailableRN();
+                //removeRole();
                 break;
             case 'View budget by department':
-                viewBudget();
+                unavailableRN();
+                //viewBudget();
                 break;
             default:
                 console.log("You didn't make a selection.");
@@ -100,6 +107,11 @@ function init(){
                 break;
         }
     });
+}
+
+function unavailableRN(){
+    console.log("Option unavailable at the time.");
+    init();
 }
 
 function viewAllEmp(){
@@ -167,8 +179,19 @@ function updateEmpRol(){
         var lastN = data.last_n;
 
         // obtain role, console log it, and confirm
+        var queryString = "SELECT role_id FROM employee WHERE ? ?";
 
-        console.log(`${firstN} ${lastN} role is`);
+        connection.query(queryString, {first_name: firstN, last_name: lastN}, function(err, res){
+            if (err) throw err;
+            //console.log(res.role_id);
+
+            var thisID = res.role_id;
+
+            connection.query("SELECT title FROM role_ WHERE ?", {id: thisID}, function(err, response){
+                if (err) throw err;
+                console.log(`${firstN} ${lastN} role is ${response.title}`); 
+            });
+        });
 
         inquirer.prompt({
             type: "confirm",
@@ -178,14 +201,33 @@ function updateEmpRol(){
             if (dataSet.changerole == true){
                 // change role
 
-                inquirer.prompt({
-                    type: "list",
-                    message: "Select new role: ",
-                    choices:['this', 'that'],
-                    name: "newrole"
-                }).then(function (dataR){
-                    var newRoleID = dataR.newrole;
+                connection.query("SELECT * FROM role_", function(err,res){
+                    if (err) throw err;
+
+                    var options = [];
+
+                    for(var i =0; i<res.length;i++){
+                        options += JSON.stringify(res[i].id + " "+ res[i].title);
+                    }
+
+                    inquirer.prompt({
+                        type: "list",
+                        message: "Select new role: ",
+                        choices: options,
+                        name: "newrole"
+                    }).then(function (dataR){
+                        var newRole = dataR.newrole.split(" ");
+                        var newRoleID = newRole[0];
+
+                        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ? last_name = ?", [newRoleID, firstN, lastN],function(err,result){
+                            if (err) throw err;
+                            console.log(`Success! ${result.first_name} new role ID is: ${result.role_id}.`); 
+                        });
+                        
+                        init();
+                    });
                 });
+                
             }
             else{
                 // initialize again
@@ -196,53 +238,53 @@ function updateEmpRol(){
     });
 }
 
-function updateEmpMan(){
-    // this function updates an employee's manager
+// function updateEmpMan(){
+//     // this function updates an employee's manager
 
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "First name of employee: ",
-            name: "first_n"
-        },
-        {
-            type: "input",
-            message: "Last name of employee: ",
-            name: "last_n"
-        }
-    ]).then(function(data){
-        var firstN = data.first_n;
-        var lastN = data.last_n;
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "First name of employee: ",
+//             name: "first_n"
+//         },
+//         {
+//             type: "input",
+//             message: "Last name of employee: ",
+//             name: "last_n"
+//         }
+//     ]).then(function(data){
+//         var firstN = data.first_n;
+//         var lastN = data.last_n;
 
-        // obtain manager from database
+//         // obtain manager from database
 
-        // log who the employee's manager is and confirm if they want to change it
-        console.log(`${firstN} ${lastN} manager is `);
+//         // log who the employee's manager is and confirm if they want to change it
+//         console.log(`${firstN} ${lastN} manager is `);
 
-        inquirer.prompt({
-            type: "confirm",
-            message: `Are you sure you would like to change ${firstN} ${lastN}'s manager?`,
-            name: "managerUp"
-        }).then(function(dataSet){
-            if (dataSet.managerUp == true){
-                // ask who's the employee's new manager
-                inquirer.prompt({
-                    type: "input",
-                    message: "New manager's id: ",
-                    name: "managID"
-                }).then(function(dataM){
-                    var managerID = dataM.managID;
-                    // get id from database and change manager
-                });
-            }
-            else{
-                // initialize again
-                console.log("Ok, no changes were made.");
-                init();
-            }
-        });
-    });
-}
+//         inquirer.prompt({
+//             type: "confirm",
+//             message: `Are you sure you would like to change ${firstN} ${lastN}'s manager?`,
+//             name: "managerUp"
+//         }).then(function(dataSet){
+//             if (dataSet.managerUp == true){
+//                 // ask who's the employee's new manager
+//                 inquirer.prompt({
+//                     type: "input",
+//                     message: "New manager's id: ",
+//                     name: "managID"
+//                 }).then(function(dataM){
+//                     var managerID = dataM.managID;
+//                     // get id from database and change manager
+//                 });
+//             }
+//             else{
+//                 // initialize again
+//                 console.log("Ok, no changes were made.");
+//                 init();
+//             }
+//         });
+//     });
+// }
 
 function addEmployee(){
     // this function adds a new employee
@@ -275,6 +317,14 @@ function addEmployee(){
         var managerID = data.manid;
 
         // Add employee
+
+        connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstN}", "${lastN}", ${roleID}, ${managerID})`, function(err, res){
+            if (err) throw err;
+
+            console.log(`Success! You have added ${firstN} ${lastN} as an employee.`);    
+        });
+        
+        init();
     });
 }
 
@@ -289,6 +339,14 @@ function addDept(){
         var deptN = data.deptname;
 
         // Add department
+
+        connection.query(`INSERT INTO department (name_) VALUES ("${deptN}")`, function(err, res){
+            if (err) throw err;
+
+            console.log(`Success! You have added ${deptN} as a new department.`); 
+        });
+        
+        init();
     });
 }
 
@@ -317,82 +375,131 @@ function addRole(){
         var deptID = data.deptid;
 
         // Add role
+
+        connection.query(`INSERT INTO role_ (title, salary, department_id) VALUES ("${role_title}", ${sal}, ${deptID})`, function(err, res){
+            if (err) throw err;
+
+            console.log(`Success! You have added ${role_title} as a new role.`); 
+        });
+        
+        init();
     });
 }
 
 function viewEmpDep(){
     // this functions shows employees by department
 
+    var depOptions = [];
+
+    connection.query("SELECT * FROM department", function(err,res){
+        if (err) throw err;
+
+        for (var i =0; i < res.length; i++){
+            depOptions += JSON.stringify(res[i].name_);
+        }
+    });
+
     inquirer.prompt({
         type: "list",
         message: "Choose a department:",
-        choices: ['this'],
+        choices: depOptions,
         name: "selectDept"
     }).then(function(data){
         var dept = data.selectDept;
 
-        // switch case by department
+        connection.query("SELECT * FROM employee WHERE department_id = ?", [dept], function(err,result){
+            if (err) throw err;
+
+            console.log(`Emploeyees in the ${dept[1]} department`);
+
+            for(var i = 0; i < result.length; i++){
+                console.log(`ID: ${result[i].id} | Name: ${result[i].first_name} ${result[i].last_name}`);
+            }
+        });
+        
+        init();
     });
 }
 
 function viewEmpRol(){
     // this function shows employees by role
 
+    var roleOptions = [];
+
+    connection.query("SELECT * FROM role_", function(err,res){
+        if (err) throw err;
+
+        for (var i =0; i < res.length; i++){
+            roleOptions += JSON.stringify(res[i].id + " " + res[i].title);
+        }
+    });
+
     inquirer.prompt({
         type: "list",
         message: "Choose a role:",
-        choices: ['this'],
+        choices: roleOptions,
         name: "selectRole"
     }).then(function(data){
-        var roleS = data.selectROle;
+        var roleS = data.selectROle.split(" ");
 
-        // switch case by role
-    });
-}
+        connection.query("SELECT * FROM employee WHERE role_id = ?", [roleS[0]], function(err,result){
+            if (err) throw err;
 
-function viewEmpMan(){
-    // this function shows employees by manager
+            console.log(`Emploeyees with the role ${roleS[1]}`);
 
-    inquirer.prompt({
-        type: "list",
-        message: "Choose a manager:",
-        choices: ['this'],
-        name: "selectMang"
-    }).then(function(data){
-        var mang = data.selectMang;
-
-        // switch case by manager
-    });
-}
-
-function removeDept(){
-    // this function removes an exiting department and all employees in such department
-
-    inquirer.prompt({
-        type: "input",
-        message: "ID of department you would like to delete: ",
-        name: "deptDel"
-    }).then(function(data){
-        var departmentDel = data.deptDel;
-
-        // obtain manager from database
-
-        inquirer.prompt({
-            type: "confirm",
-            message: `Are you sure you would like to remove ${departmentDel} and all employees and roles in that department?`,
-            name: "removeDept"
-        }).then(function(dataSet){
-            if (dataSet.removeDept == true){
-                // remove whole department
-            }
-            else{
-                // initialize again
-                console.log("Ok, no changes were made.");
-                init();
+            for(var i = 0; i < result.length; i++){
+                console.log(`ID: ${result[i].id} | Name: ${result[i].first_name} ${result[i].last_name}`);
             }
         });
+        
+        init();
     });
 }
+
+// function viewEmpMan(){
+//     // this function shows employees by manager
+
+//     inquirer.prompt({
+//         type: "list",
+//         message: "Choose a manager:",
+//         choices: ['this'],
+//         name: "selectMang"
+//     }).then(function(data){
+//         var mang = data.selectMang;
+
+//         // switch case by manager
+//     });
+// }
+
+// function removeDept(){
+//     // this function removes an exiting department and all employees in such department
+
+//     inquirer.prompt({
+//         type: "input",
+//         message: "ID of department you would like to delete: ",
+//         name: "deptDel"
+//     }).then(function(data){
+//         var departmentDel = data.deptDel;
+
+//         // obtain manager from database
+
+//         inquirer.prompt({
+//             type: "confirm",
+//             message: `Are you sure you would like to remove the ${departmentDel} department and all employees and roles in this department?`,
+//             name: "removeDept"
+//         }).then(function(dataSet){
+//             if (dataSet.removeDept == true){
+//                 // remove whole department
+//                 console.log(`Removed ${departmentDel} department.`);
+//             }
+//             else{
+//                 // initialize again
+//                 console.log("Ok, no changes were made.");
+//                 init();
+//             }
+//         });
+//     });
+// }
 
 function removeEmp(){
     // this function removes an exiting employee
@@ -421,6 +528,12 @@ function removeEmp(){
         }).then(function(dataSet){
             if (dataSet.removeE == true){
                 // remove emp
+
+                connection.query("DELETE FROM employee WHERE first_name = ? last_name = ?", [firstN,lastN], function(err, res){
+                    if (err) throw err;
+                    console.log(`Employee ${firstN} ${lastN} has been removed from the database.`)
+                });
+                init();
             }
             else{
                 // initialize again
@@ -431,35 +544,35 @@ function removeEmp(){
     });
 }
 
-function removeRole(){
-    // this function removes an exiting role and all employees with such role
+// function removeRole(){
+//     // this function removes an exiting role and all employees with such role
 
-    inquirer.prompt({
-        type: "input",
-        message: "ID of role you would like to delete: ",
-        name: "roleDel"
-    }).then(function(data){
-        var roleDelete = data.roleDel;
+//     inquirer.prompt({
+//         type: "input",
+//         message: "ID of role you would like to delete: ",
+//         name: "roleDel"
+//     }).then(function(data){
+//         var roleDelete = data.roleDel;
 
-        // obtain manager from database
+//         // obtain manager from database
 
-        inquirer.prompt({
-            type: "confirm",
-            message: `Are you sure you would like to remove ${roleDelete} and all employees and roles in that department?`,
-            name: "removeRole"
-        }).then(function(dataSet){
-            if (dataSet.removeRole == true){
-                // remove whole department
-            }
-            else{
-                // initialize again
-                console.log("Ok, no changes were made.");
-                init();
-            }
-        });
-    });
-}
+//         inquirer.prompt({
+//             type: "confirm",
+//             message: `Are you sure you would like to remove ${roleDelete} and all employees and roles in that department?`,
+//             name: "removeRole"
+//         }).then(function(dataSet){
+//             if (dataSet.removeRole == true){
+//                 // remove whole department
+//             }
+//             else{
+//                 // initialize again
+//                 console.log("Ok, no changes were made.");
+//                 init();
+//             }
+//         });
+//     });
+// }
 
-function viewBudget(){
-    // this function shows the budget of a specific department
-}
+// function viewBudget(){
+//     // this function shows the budget of a specific department
+// }
